38a39ff6430b198cc9d353a8a683e073fb4a4eb3
Heby T Paul <hebytpaul1111@gmail.com>
2023-06-30 04:11:21 +0530

    redesign Note page

--- src/Pages/Note.jsx
+++ src/Pages/Note.jsx
@@ -29,10 +29,10 @@ function Note() {
       <div className=" h-[calc(100vh-5.9rem)] m-4 p-6 bg-white bg-opacity-60 rounded-lg shadow-md ">
         {/* <h1 className="text-2xl font-bold mb-4 text-center">Note {note.id}</h1> */}
         <div className="mb-4">
-          <input type="text" id="title" name="title" value={note.title} onChange={handleTitleChange} placeholder="Title" className="w-full bg-transparent border-b border-gray-300 focus:border-gray-400 text-xl font-medium text-gray-700 focus:outline-none focus:shadow-sm text-center rounded " />
+          <input type="text" id="title" name="title" value={note.title} onChange={handleTitleChange} placeholder="Title" className="w-full bg-transparent border-b border-gray-300 focus:border-gray-400 text-xl font-medium text-gray-700 focus:outline-none focus:shadow-sm  rounded " />
         </div>
         <div>
-          <textarea id="content" name="content" rows={10} value={note.content} onChange={handleContentChange} placeholder="Content" className="w-full bg-transparent border-b border-gray-300 focus:border-gray-400 text-base font-normal text-gray-700 focus:outline-none focus:shadow-sm text-center rounded"></textarea>
+          <textarea id="content" name="content" rows={10} value={note.content} onChange={handleContentChange} placeholder="Start writing ... " className="w-full bg-transparent border-b border-gray-300 focus:border-gray-400 text-base font-normal text-gray-700 focus:outline-none focus:shadow-sm  rounded"></textarea>
         </div>
       </div>
     </Layout>
