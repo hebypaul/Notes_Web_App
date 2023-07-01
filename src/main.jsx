20c70fdef347292d009fea5ee706594e27db2366
Heby T Paul <hebytpaul1111@gmail.com>
2023-07-01 23:24:30 +0530

    Appwrite Initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { AppwriteContext ,client } from './Appwrite/AppwriteContext.js'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
-    <App/>
+    <AppwriteContext.Provider value={client}>
+      <App/>
+    </AppwriteContext.Provider>
   </React.StrictMode>,
 )
